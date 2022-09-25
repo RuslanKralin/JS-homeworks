a430e2f45f35de77372ff699930a7fd046504bff
kralin.front <kralin.front@gmail.com>
2022-09-25 20:12:22 +0300

    updete 2lessons

---
+++ lesson2/script.js
@@ -0,0 +1,17 @@
+//задание 1
+let name, age, city, phone, email, company;
+name = prompt ('Ваше имя?', 'Руслан');
+age = prompt ('Ваш возраст?', '39');
+city = prompt ('где вы проживаете?', 'г.Минск');
+phone = prompt ('ваш номер телефона', '+375297888192');
+email = prompt ('ваша почта', 'kralin.front@gmail.com');
+company = prompt ('где вы работаете?', 'ООО Рога и копыта');
+
+//задание 2
+let year = 2022-age;
+document.write(name,' родился в',' ', year , ' году');
+
+
+
+
+
