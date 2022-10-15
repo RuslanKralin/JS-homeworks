2adee594804573b44358df55eaa214cb0786e1f5
kralin.front <kralin.front@gmail.com>
2022-10-15 12:45:40 +0300

    WB

---
+++ weekly_Rus/mainW.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', () => {
+    const formInputField = document.querySelector('.inputField');
+    const inputGeneral = document.querySelector('.inputGeneral');
+    const btnInputField = document.querySelector('.inputField-btn');
+    const budget = document.querySelector('#budget');
+
+   
+
+    formInputField.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('')
+        const moneyBudgetInput = inputGeneral;//писваиваем точному значению инпут
+
+        
+        
+        // if(!moneyBudgetInput){
+        //     alert('Please fill out money');
+        //     return;//чтоб ничего ещё не произошло нужно вернуться
+        // } 
+
+    
+       
+       
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
