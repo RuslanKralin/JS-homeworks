61c6ba7a1643509e17354a2455ef66105f01c196
kralin.front <kralin.front@gmail.com>
2022-10-21 19:19:14 +0300

    update WB

--- weekly_Rus/mainW.js
+++ weekly_Rus/mainW.js
@@ -4,75 +4,116 @@ const btnInputField = document.querySelector(".inputField-btn");
 const budget = document.querySelector("#budget");
 const balance = document.querySelector("#balance");
 
+const addProduct = document.querySelector(".add");
+const inputName = document.querySelector(".inputName");
+const inputPrice = document.querySelector(".inputPrice");
+const ul = document.querySelector(".nameProductList");
 
+let productListData = []; //для локал стор
+
+
+if (window.location.pathname == "/weekly_Rus/") {
+  if (localStorage.getItem("productListData") !== null) {
+    let localProductData = localStorage.getItem("productListData") || [];
+    if (localProductData.length > 0)
+      productListData = JSON.parse(localProductData);
+
+    ul.innerHTML = "";
+
+    productListData.forEach(function (newProduct) {
+      let productElement = document.createElement("li");
+      productElement.innerHTML = `<li class="nameProduct">
+      ${newProduct.name}:
+       ${newProduct.price} $</li>
+      `;
+      ul.appendChild(productElement);
+    });
+
+    // productUpdate();
+  }
+}
 
 formInputField.children[1].addEventListener("click", (e) => {
   e.preventDefault(e);
-  if (
-    /^\d{1,10}$/g.test(formInputField.children[0].value) 
-    
-  ){
+  if (/^\d{1,10}$/g.test(formInputField.children[0].value)) {
     budget.append(+formInputField.children[0].value);
     balance.append(+formInputField.children[0].value);
     formInputField.style.display = "none";
+
+    // let Bud = budget.textContent;
+    // localStorage.setItem('budgetData', JSON.stringify(Bud));
+    // let budgetData = localStorage.getItem("Bud")
+    // Bud = JSON.parse(budgetData)
   }
+});
 
-  
- });
-
-btnInputField.addEventListener("click", () => { 
-  if(!inputGeneral.value){
-      alert('Give Me Money!');
-      return;//чтоб ничего ещё не произошло нужно вернуться
+btnInputField.addEventListener("click", () => {
+  if (!inputGeneral.value) {
+    alert("Give Me Money!");
+    return; //чтоб ничего ещё не произошло нужно вернуться
   }
- });
+});
 
-const addProduct = document.querySelector('.add');
-const inputName = document.querySelector('.inputName');
-const inputPrice = document.querySelector('.inputPrice');
-const ul = document.querySelector(".nameProductList");
-
-addProduct.addEventListener('click', (e) => {
+addProduct.addEventListener("click", (e) => {
   e.preventDefault(e);
-    if(
-      inputName.value && 
-      /^\d{1,10}$/g.test(inputPrice.value) &&
+  //валидация на заполнение полей
+  if (
+    inputName.value &&
+    /^\d{1,10}$/g.test(inputPrice.value) &&
     budget.textContent !== ""
-  ){
-    makeLi(inputName.value, inputPrice.value);
+  ) {
+    //создаём объект для продукта, который добавим в массив
+    let newProduct = {
+      name: inputName.value,
+      price: +inputPrice.value,
+    };
+
+    makeLi(newProduct.name, newProduct.price); //объявляем функцию со значениями нового имени и новой цены на добавленный продукт
     newBalance(inputPrice.value);
     lowBalance(balance.textContent);
+
+    productListData.push(newProduct);
+
     inputName.value = "";
     inputPrice.value = "";
+
+    localStorage.setItem("productListData", JSON.stringify(productListData)); //ключ и значение/добавили в локал стор
+
+    //productUpdate();
   }
- })
+});
 
-function makeLi(inputName, inputPrice){
-  let li = document.createElement('li');
-  li.innerHTML = `${inputName}:  ${inputPrice}$;`
+let localProductData = localStorage.getItem("productListData") || [];
+
+if (localProductData.length > 0) productListData = JSON.parse(localProductData);
+
+ul.innerHTML = "";
+
+productListData.forEach(function (newProduct) {
+  let productElement = document.createElement("li");
+  productElement.innerHTML = `<li class="nameProduct">
+    ${newProduct.name}:
+     ${newProduct.price} $</li>
+    `;
+  ul.appendChild(productElement);
+});
+
+function makeLi(inputName, inputPrice) {
+  let li = document.createElement("li");
+  li.innerHTML = `${inputName}:  ${inputPrice}$;`;
   ul.append(li);
-  
 }
- function newBalance(inputPrice){
-   balance.innerHTML = (balance.textContent - inputPrice).toFixed(2);
- }
-
- const massege = document.querySelector('.extraMassege');
-const left = document.querySelector('.left');
-
-function lowBalance(balance){
-  if(balance <0){
-  massege.removeAttribute("none"); 
-  massege.style.display = 'block';
-  left.style.background = color = 'red'
-  // left {
-  //   animation: left 2s infinite; /* Параметры анимации */
-  //  }
-  //  @keyframes left {
-  //   from { opacity: 1; /* Непрозрачный текст */ }
-  //   to { opacity: 0; /* Прозрачный текст */ }
-  //  }
-  
-  
+function newBalance(inputPrice) {
+  balance.innerHTML = (balance.textContent - inputPrice).toFixed(2);
 }
+
+const massege = document.querySelector(".extraMassege");
+const left = document.querySelector(".left");
+
+function lowBalance(balance) {
+  if (balance < 0) {
+    massege.removeAttribute("none");
+    massege.style.display = "block";
+    left.style.background = color = "red";
+  }
 }
